ddc27f60fdf48a7b9b2e977d64ba207b902ca386
TomasMarina <tomasimarina@gmail.com>
2022-07-13 18:24:21 -0300

    Relation QSS dif - spp attr

--- R/QSSdif_Sppattr.R
+++ R/QSSdif_Sppattr.R
@@ -1,121 +1,92 @@
 
-library(ggplot2)
 library(dplyr)
+library(ggplot2)
 
 ## Load data
-data <- readRDS(file = "Results/QSS_extinction_dif.rds")
+
+load("Results/QSS_extinction_dif.rda")
 load("Results/network_&_spp_attr.rda")
 
 
-## Habitat data
+## Compare QSS empirical and null
+
+ggplot(QSS_null_comp_raw, aes(x = maxre, color = network)) +
+  geom_histogram() +
+  labs(x = "QSS eigenvalues", y = "Frequency", color = "Network") +
+  theme_classic()
+
+
+## Merge QSS results with spp attributes
+
+# Habitat data
 hab_data <- read.csv(file = "Data/WeddellSeaHabitat.csv")
 hab_data_inc <- hab_data %>% 
   dplyr::select(species, Environment) %>% 
   rename(TrophicSpecies = species, Habitat = Environment)
 hab_data_comp <- hab_data_inc %>% 
-  add_row(TrophicSpecies = "Phytodetritus", Habitat = "Benthic") %>%        # missing species
-  add_row(TrophicSpecies = "Sediment", Habitat = "Benthic") %>%             # missing species
-  add_row(TrophicSpecies = "Iphimediella  cyclogena", Habitat = "Benthic")  # missing species
+  add_row(TrophicSpecies = "Phytodetritus", Habitat = "Benthic") %>%            # missing species
+  add_row(TrophicSpecies = "Sediment", Habitat = "Benthic") %>%                 # missing species
+  add_row(TrophicSpecies = "Iphimediella  cyclogena", Habitat = "Benthic") %>%  # missing species
+  mutate(Habitat = case_when(Habitat == "Bathydemersal" ~ "Demersal", TRUE ~ Habitat))  # replace 'Bathydemersal' with 'Demersal'
 
+# Join extinction results (QSS), Habitat & spp properties
+QSS_data <- QSS_extinction_dif
 
-## Join extinction results (QSS), Habitat & spp properties
-all_data <- data %>% 
+all_data <- QSS_data %>% 
   rename(TrophicSpecies = Deleted) %>% 
   left_join(spp_attr_all) %>% 
   left_join(hab_data_comp)
 
 
-## Correlation p-values (Kolmogorov vs Anderson Darling)
-ggplot(data, aes(x = KS_pvalue, y = Ad_pvalue)) +
-  geom_point()
-
-
 ## Relationship btw QSS difference and spp properties
+# Positive values indicate spp that are destibilizers
+# Negative values indicate spp that are stibilizers
+
+# Correlation p-values (Kolmogorov vs Anderson Darling)
+ggplot(all_data, aes(x = KS_pvalue, y = Ad_pvalue)) +
+  geom_point() +
+  labs(x = "Kolmogorov-Smirnov p-value", y = "Anderson-Darling p-value") +
+  theme_classic()
+
+# By trophic level
 ggplot(all_data, aes(x = TLu, y = difQSS)) +
-  geom_point()
+  geom_point(aes(color = ifelse(Ad_pvalue < 0.01, "Significat", "Non-significant"))) +
+  labs(color = "AD p-value", x = "Trophic level", y = "QSS difference") +
+  theme_classic()
 
+# By degree
 ggplot(all_data, aes(x = Degree, y = difQSS)) +
-  geom_point() +
-  scale_x_log10()
+  geom_point(aes(color = ifelse(Ad_pvalue < 0.01, "Significat", "Non-significant"))) +
+  scale_x_log10() +
+  labs(color = "AD p-value", x = "log Degree", y = "QSS difference") +
+  theme_classic()
 
+# By mean interaction strength
 ggplot(all_data, aes(x = AllStrength_mean, y = difQSS)) +
-  geom_point() +
-  scale_x_log10()
+  geom_point(aes(color = ifelse(Ad_pvalue < 0.01, "Significat", "Non-significant"))) +
+  scale_x_log10() +
+  labs(color = "AD p-value", x = "log mean IS", y = "QSS difference") +
+  theme_classic()
 
+# By trophic similarity
 ggplot(all_data, aes(x = meanTrophicSimil, y = difQSS)) +
-  geom_point()
+  geom_point(aes(color = ifelse(Ad_pvalue < 0.01, "Significat", "Non-significant"))) +
+  labs(color = "AD p-value", x = "Trophic similarity", y = "QSS difference") +
+  theme_classic()
 
+# By omnivory
 ggplot(all_data, aes(x = Omnu, y = difQSS)) +
-  geom_point() +
-  scale_x_log10()
+  geom_point(aes(color = ifelse(Ad_pvalue < 0.01, "Significat", "Non-significant"))) +
+  labs(color = "AD p-value", x = "Omnivory", y = "QSS difference") +
+  theme_classic()
 
+# By habitat
 ggplot(all_data, aes(x = Habitat, y = difQSS)) +
-  geom_boxplot()
-
-
-## 20 + 20 influencers
-
-# Destabilisers by presence
-des_spp_pres <- all_data %>% 
-  arrange(desc(difQSS)) %>% 
-  slice(1:20) %>% 
-  mutate(Destabilization = "Presence")
-# Destabilisers by abscence
-des_spp_abs <- all_data %>% 
-  arrange(desc(-difQSS)) %>% 
-  slice(1:20) %>% 
-  mutate(Destabilization = "Abscence")
-
-inf_spp <- bind_rows(des_spp_pres, des_spp_abs)
-
-
-ggplot(inf_spp, aes(x = TLu, y = difQSS)) +
-  geom_point()
-
-ggplot(inf_spp, aes(x = Habitat, y = difQSS)) +
-  geom_point()
-
-ggplot(inf_spp, aes(x = meanTrophicSimil, y = difQSS)) +
-  geom_point()
-
-ggplot(inf_spp, aes(x = Degree, y = difQSS)) +
-  geom_point()
-
-ggplot(inf_spp, aes(x = AllStrength_mean, y = difQSS)) +
-  geom_point() +
-  scale_x_log10()
-
-
-ggplot(wedd_int, aes(qRC)) +
-  geom_histogram()
-
-inf_spp_name <- inf_spp$TrophicSpecies
-inf_spp_dist <- all_int[all_int$TrophicSpecies %in% inf_spp_name, ] %>% 
-  left_join(inf_spp)
-unique(inf_spp_dist$TrophicSpecies)  
-
-library(ggjoy)
-p <- ggplot(inf_spp_dist, aes(x = IS, y = reorder(TrophicSpecies, TLu, decreasing=T))) +
-  geom_density_ridges(aes(fill = Destabilization), jittered_points = TRUE) +
-  scale_x_log10()
-p + labs(fill = "Destabilization")
-
-ggplot(inf_spp, aes(y = TLu)) +
-  geom_boxplot() +
-  geom_jitter()
-
-
-## GAM
-
-library(mgcv)
-library(gratia)
-
-all_fit <- gam(difQSS ~ te(TLu, Degree, k=c(8,8)), data = all_data)
-plot(all_fit, rug=F, pers=T, theta=45, main="Strength")
-draw(all_fit, residuals=T) 
-appraise(all_fit) 
-gam.check(all_fit)
-summary(all_fit)  # percentage of deviance explained
+  geom_violin(fill = "orange") +
+  geom_point(aes(color = ifelse(Ad_pvalue < 0.01, "Significat", "Non-significant"))) +
+  scale_color_manual(values = c("black", "blue")) +
+  labs(x = "Habitat", y = "QSS difference", color = "AD p-value") +
+  theme_classic()
 
 
 
