ceb8285556fdc90029f152839f6cddddfed6b2d2
TomasMarina <tomasimarina@gmail.com>
2022-07-28 13:01:26 -0300

    Clean up figures

--- R/gam_InteractionStrength.R
+++ R/gam_InteractionStrength.R
@@ -23,6 +23,7 @@ load("Results/network_&_spp_attr.rda")
 # Trophic level, degree & mean trophic similarity
 fit_full <- gam(AllStrength_mean ~ te(TLu, Degree, meanTrophicSimil, k=c(6,6,6)), data = spp_attr_all)
 summary(fit_full)
+de_full <- summary(fit_full)$dev.expl
 gam.check(fit_full)
 draw(fit_full)
 appraise(fit_full) 
@@ -30,6 +31,7 @@ appraise(fit_full)
 # Full model no interaction
 fit_fnint <- gam(AllStrength_mean ~ s(TLu) + s(Degree) + s(meanTrophicSimil), data = spp_attr_all)
 summary(fit_fnint)
+de_fnint <- summary(fit_fnint)$dev.expl
 gam.check(fit_fnint)
 draw(fit_fnint)
 appraise(fit_fnint) 
@@ -38,13 +40,17 @@ appraise(fit_fnint)
 
 fit_tldeg <- gam(AllStrength_mean ~ te(TLu, Degree, k=c(6,6)), data = spp_attr_all)
 summary(fit_tldeg)
+de_tldeg <- summary(fit_tldeg)$dev.expl
 gam.check(fit_tldeg)
 draw(fit_tldeg)
-appraise(fit_tldeg) 
+appraise(fit_tldeg)
+plot(fit_tldeg, rug=F, scheme=T, theta=45, 
+     main="Interaction Strength", xlab="Trophic level", ylab="Degree")
 
 # TL & Degree no interaction
 fit_tldeg_nint <- gam(AllStrength_mean ~ s(TLu) + s(Degree), data = spp_attr_all)
 summary(fit_tldeg_nint)
+de_tldeg_nint <- summary(fit_tldeg_nint)$dev.expl
 gam.check(fit_tldeg_nint)
 draw(fit_tldeg_nint)
 appraise(fit_tldeg_nint) 
@@ -53,6 +59,7 @@ appraise(fit_tldeg_nint)
 
 fit_tlts <- gam(AllStrength_mean ~ te(TLu, meanTrophicSimil, k=c(6,6)), data = spp_attr_all)
 summary(fit_tlts)
+de_tlts <- summary(fit_tlts)$dev.expl
 gam.check(fit_tlts)
 draw(fit_tlts)
 appraise(fit_tlts) 
@@ -60,6 +67,7 @@ appraise(fit_tlts)
 # TL & TS no interaction
 fit_tlts_nint <- gam(AllStrength_mean ~ s(TLu) + s(meanTrophicSimil), data = spp_attr_all)
 summary(fit_tlts_nint)
+de_tlts_nint <- summary(fit_tlts_nint)$dev.expl
 gam.check(fit_tlts_nint)
 draw(fit_tlts_nint)
 appraise(fit_tlts_nint)
@@ -68,6 +76,7 @@ appraise(fit_tlts_nint)
 
 fit_degts <- gam(AllStrength_mean ~ te(Degree, meanTrophicSimil, k=c(6,6)), data = spp_attr_all)
 summary(fit_degts)
+de_degts <- summary(fit_degts)$dev.expl
 gam.check(fit_degts)
 draw(fit_degts)
 appraise(fit_degts) 
@@ -75,6 +84,7 @@ appraise(fit_degts)
 # TL & TS no interaction
 fit_degts_nint <- gam(AllStrength_mean ~ s(Degree) + s(meanTrophicSimil), data = spp_attr_all)
 summary(fit_degts_nint)
+de_degts_nint <- summary(fit_degts_nint)$dev.expl
 gam.check(fit_degts_nint)
 draw(fit_degts_nint)
 appraise(fit_degts_nint)
@@ -86,36 +96,30 @@ delta_aic <- tibble(AIC(fit_full, fit_fnint, fit_tldeg, fit_tldeg_nint,
                         fit_tlts, fit_tlts_nint, fit_degts, fit_degts_nint))
 delta_aic$model <- rownames(AIC(fit_full, fit_fnint, fit_tldeg, fit_tldeg_nint,
                                 fit_tlts, fit_tlts_nint, fit_degts, fit_degts_nint))
-
-delta_aic %>% mutate(delta_AIC = AIC - min(AIC) ) %>% arrange(delta_AIC)
-
-
-# Use weighted TL
-fitw <- gam(TotalStrength ~ te(TLw, Degree, k=c(8,8)), data = spp_attr)
-plot(fitw, rug=F, pers=T, theta=45, main="Strength")
-draw(fitw, residuals=T) 
-appraise(fitw) 
-gam.check(fitw)
-summary(fitw)  # percentage of deviance explained
+delta_aic$dev.expl <- c(de_full, de_fnint, de_tldeg, de_tldeg_nint, de_tlts,
+                        de_tlts_nint, de_degts, de_degts_nint)
+delta_aic %>% mutate(delta_AIC = AIC - min(AIC) ) %>% 
+  arrange(delta_AIC) %>% dplyr::select(model, dev.expl, AIC, df, delta_AIC)
 
 
-fit_dt <- gam(TotalStrength ~ te( Degree, meanTrophicSimil, k=c(6,6)), data = spp_attr)
-summary(fit_dt)  # percentage of deviance explained
-appraise(fit_dt) 
+# Plot GAM ----
 
-fit_tt <- gam(TotalStrength ~ te( TLu, meanTrophicSimil, k=c(6,6)), data = spp_attr)
-summary(fit_tt)  # percentage of deviance explained
-appraise(fit_dt) 
+str(spp_attr_all)
+v <- ggplot(spp_attr_all, aes(x = Degree, y = TLu, z = AllStrength_mean))
+v + geom_contour_filled()
+v + geom_raster(aes(fill = AllStrength_mean)) +
+  geom_contour(colour = "white")
 
-# Use unweighted (topological) TL
-fitu <- gam(TotalStrength ~ te(TLu, Degree, k=c(8,8)), data = spp_attr)
-plot(fitu, rug=F, scheme=T, theta=45, main="Strength")
-draw(fitu, residuals=T) 
-appraise(fitu) 
-gam.check(fitu)
-summary(fitu)
+plot.gam(fit_full, pages=1, residuals=TRUE, all.terms=TRUE, shade=TRUE, shade.col=2)
+plot.gam(fit_full, pages=1, seWithMean=TRUE)
 
+library(mgcViz)
+b <- getViz(fit_full)
+plotRGL(sm(b, 1), fix = c("z" = 0), residuals = TRUE)
 
+str(faithfuld)
+g <- ggplot(faithfuld, aes(waiting, eruptions, z = density))
+g + geom_contour_filled()
 
 
 
@@ -124,35 +128,35 @@ summary(fitu)
 # Plot GAM model and observed data
 # Taken from https://stackoverflow.com/questions/55047365/r-plot-gam-3d-surface-to-show-also-actual-response-values
 # 2D plot
-fitu_plot <- gam((TotalStrength) ~ te(TLu, Degree, k=c(6,6)), data = spp_attr,family=tw)
-
-# fitu_plot <- gam(TotalStrength ~ te(TLu, Degree,), data = spp_attr)  # not work with 'family=tw'
+fitu_plot <- gam((AllStrength_mean) ~ te(TLu, Degree, k=c(6,6)), data = spp_attr_all)  # not work with 'family=tw'
 summary(fitu_plot)  # deviance % explained by the model
 appraise(fitu_plot) 
 
 # Now expand it to a grid so that persp will work
 steps <- 30
-Degree <- with(spp_attr, seq(min(Degree), max(Degree), length = steps) )
-TLu <-  with(spp_attr, seq(min(TLu), max(TLu), length = steps) )
-#meanTrophicSimil <- with(spp_attr, seq(min(meanTrophicSimil), max(meanTrophicSimil), length = 6) )
+Degree <- with(spp_attr_all, seq(min(Degree), max(Degree), length = steps) )
+TLu <-  with(spp_attr_all, seq(min(TLu), max(TLu), length = steps) )
+#meanTrophicSimil <- with(spp_attr_all, seq(min(meanTrophicSimil), max(meanTrophicSimil), length = 6))
 newdat <- expand.grid(Degree = Degree, TLu = TLu)
 ilink <- family(fitu_plot)$linkinv 
 
-TotalStrength <- matrix(predict(fitu_plot, newdat), steps, steps)
-TotalStrength <- ilink(TotalStrength)
+AllStrength_mean <- matrix(predict(fitu_plot, newdat), steps, steps)
+AllStrength_mean <- ilink(AllStrength_mean)
 
 # Now plot it
-p <- persp(Degree, TLu, TotalStrength, theta = 10, col = alpha("yellow", 0.2), 
-           xlim = range(Degree), ylim = range(TLu), zlim = range(TotalStrength),
-           xlab = "Degree", ylab = "Trophic level", zlab = "Interaction strength")
+p <- persp(Degree, TLu, AllStrength_mean, theta = 10, col = alpha("yellow", 0.2), 
+           xlim = range(Degree), ylim = range(TLu), zlim = range(AllStrength_mean),
+           xlab = "Degree", ylab = "Trophic level", zlab = "mean Interaction strength")
 
 # To add the points, you need the same 3d transformation
-obs <- with(spp_attr , trans3d(Degree, TLu, (TotalStrength), p))
-pred <- with(spp_attr, trans3d(Degree, TLu, fitted(fitu_plot), p))
+obs <- with(spp_attr_all, trans3d(Degree, TLu, (AllStrength_mean), p))
+pred <- with(spp_attr_all, trans3d(Degree, TLu, fitted(fitu_plot), p))
 dif <- obs[["y"]] - pred[["y"]]  # observed - predicted strength
 points(obs, col = ifelse(dif < 0, alpha("blue",0.4), alpha("red",1)), pch = 16)
 # Add segments to show where the points are in 3d
 segments(obs$x, obs$y, pred$x, pred$y)
+
+
 
 require(plotly)
 plot_ly() %>% 
--- R/quantile_regression.R
+++ R/quantile_regression.R
@@ -26,42 +26,24 @@ load("Results/network_&_spp_attr.rda")
 attach(spp_attr_all)
 datatable <- data.frame(Degree, TLu, Omnu, meanTrophicSimil, 
                         AllStrength_sum, AllStrength_mean)
-
 cor(datatable)
 pairs(datatable, col="blue", main="Scatterplots")
 
 
 ## Trophic level ----
 
-IntStr <- cbind(AllStrength_mean)
-TL <- cbind(TLu)
+# Test
+QR <- rq(log(AllStrength_mean) ~ TLu, tau=c(0.15, 0.85), data=spp_attr_all)
+summary(QR, se= "boot")
+QR_15 <- rq(log(AllStrength_mean) ~ TLu, tau=0.15, data=spp_attr_all)
+QR_85 <- rq(log(AllStrength_mean) ~ TLu, tau=0.85, data=spp_attr_all)
+anova(QR_15, QR_85)  # test difference btw quantiles 15 & 85
 
-hist(IntStr, prob=TRUE, col = "blue", border = "black", xlab = "mean Interaction Strength")
-lines(density(IntStr))
-
-# Lineal regression
-OLSreg <- lm(IntStr ~ TL)
-summary(OLSreg)
-
-# Quantile 25 & 75
-QR_2575 <- rq(IntStr ~ TL, tau=c(0.15, 0.85))
-sumQR_2575_boot <- summary(QR_2575, se = "boot")  # test significance of regression
-
-QR_25 <- rq(IntStr ~ TL, tau=0.15)
-QR_75 <- rq(IntStr ~ TL, tau=0.85)
-anova(QR_25, QR_75)  # test difference btw quantiles 25 & 75
-
-
-# Scatter plot with quantile regression lines
-
-q <- c(0.15, 0.85)
-
+# Plot
 qr_IS_TL <- ggplot(spp_attr_all, aes(x = TLu, y = log(AllStrength_mean))) +
-  # geom_errorbar(aes(ymin = AllStrength_mean-AllStrength_Q1, ymax = AllStrength_mean+AllStrength_Q3), width = .1, position = pd) +
   geom_point() + 
-  geom_quantile(quantiles = q, size = 2, alpha = 0.5, aes(colour = as.factor(..quantile..))) +
-  #scale_y_log10() +
-  labs(x = "Trophic level", y = "mean Interaction Strength", color = "Quantiles") +
+  geom_quantile(quantiles = c(0.15, 0.85), size = 2, alpha = 0.5, aes(colour = as.factor(..quantile..))) +
+  labs(x = "Trophic level", y = "mean Interaction Strength (log scale)", color = "Quantiles") +
   theme_bw() +
   theme(panel.grid = element_blank(),
         axis.title = element_text(size = 18, face = "bold"),
@@ -70,72 +52,48 @@ qr_IS_TL <- ggplot(spp_attr_all, aes(x = TLu, y = log(AllStrength_mean))) +
 qr_IS_TL
 
 
-## TL ----
-QR <- rq(log(AllStrength_mean) ~ TLu, tau=c(0.15, 0.85),data=spp_attr_all)
-summary(QR, se= "boot")
-
-
-QR_15 <- rq(log(AllStrength_mean) ~ TLu, tau=0.15,data=spp_attr_all)
-QR_85 <- rq(log(AllStrength_mean) ~ TLu, tau=0.85,data=spp_attr_all)
-anova(QR_15, QR_85)  # test difference btw quantiles 25 & 75
-
-ggplot(spp_attr_all, aes(x = TLu, y = log(AllStrength_mean))) +
-  geom_quantile(quantiles = q, size = 2, alpha = 0.5, aes(colour = as.factor(..quantile..))) +
-  geom_point() +
-  # scale_y_log10() +
-  # scale_x_log10() +
-  labs(x = "Trophic Level", y = "Interaction strength (log scale)") +
-  theme_bw() +
-  theme(panel.grid = element_blank(),
-        axis.title = element_text(size = 18, face = "bold"),
-        axis.text.x = element_text(size = 15),
-        axis.text.y = element_text(size = 15))
-
-
 ## Degree ----
-QRD_15 <- rq(log(AllStrength_mean) ~ Degree, tau=0.15,data=spp_attr_all)
-QRD_85 <- rq(log(AllStrength_mean) ~ Degree, tau=0.85,data=spp_attr_all)
-anova(QRD_15, QRD_85)  # test difference btw quantiles 25 & 75
 
-QRD <- rq(log(AllStrength_mean) ~ Degree, tau=c(0.15, 0.85),data=spp_attr_all)
+# Test
+QRD <- rq(log(AllStrength_mean) ~ Degree, tau=c(0.15, 0.85), data=spp_attr_all)
 summary(QRD, se= "boot")
+QRD_15 <- rq(log(AllStrength_mean) ~ Degree, tau=0.15, data=spp_attr_all)
+QRD_85 <- rq(log(AllStrength_mean) ~ Degree, tau=0.85, data=spp_attr_all)
+anova(QRD_15, QRD_85)
 
-
-ggplot(spp_attr_all, aes(x = Degree, y = log(AllStrength_mean))) +
-  geom_quantile(quantiles = q, size = 2, alpha = 0.5, aes(colour = as.factor(..quantile..))) +
+# Plot
+qr_IS_DEG <- ggplot(spp_attr_all, aes(x = Degree, y = log(AllStrength_mean))) +
   geom_point() +
-  # scale_y_log10() +
-  # scale_x_log10() +
-  labs(x = "Degree", y = "Interaction strength (log scale)") +
+  geom_quantile(quantiles = c(0.15, 0.85), size = 2, alpha = 0.5, aes(colour = as.factor(..quantile..))) +
+  labs(x = "Degree", y = "Interaction strength (log scale)", color = "Quantiles") +
   theme_bw() +
   theme(panel.grid = element_blank(),
         axis.title = element_text(size = 18, face = "bold"),
         axis.text.x = element_text(size = 15),
         axis.text.y = element_text(size = 15))
+qr_IS_DEG
 
 
-## Trophic Simil
+## Trophic Simil ----
 
-QRS_15 <- rq(log(AllStrength_mean) ~ meanTrophicSimil, tau=0.15,data=spp_attr_all)
-QRS_85 <- rq(log(AllStrength_mean) ~ meanTrophicSimil, tau=0.85,data=spp_attr_all)
-anova(QRS_15, QRS_85)  # test difference btw quantiles 25 & 75
+# Test
+QRTS <- rq(log(AllStrength_mean) ~ meanTrophicSimil, tau=c(0.15,0.5, 0.85), data=spp_attr_all)
+summary(QRTS, se= "boot")
+QRS_15 <- rq(log(AllStrength_mean) ~ meanTrophicSimil, tau=0.15, data=spp_attr_all)
+QRS_85 <- rq(log(AllStrength_mean) ~ meanTrophicSimil, tau=0.85, data=spp_attr_all)
+anova(QRS_15, QRS_85)
 
-QRD <- rq(log(AllStrength_mean) ~ meanTrophicSimil, tau=c(0.15,0.5, 0.85),data=spp_attr_all)
-summary(QRD, se= "boot")
-
-q <- c(0.15, 0.85)
-
-ggplot(spp_attr_all, aes(x = meanTrophicSimil, y = log(AllStrength_mean))) +
-  geom_quantile(quantiles = q, size = 2, alpha = 0.5, aes(colour = as.factor(..quantile..))) +
+# Plot
+qr_IS_TS <- ggplot(spp_attr_all, aes(x = meanTrophicSimil, y = log(AllStrength_mean))) +
   geom_point() +
-  # scale_y_log10() +
-  # scale_x_log10() +
-  labs(x = "Mean Trophic Similitude", y = "Interaction strength (log scale)") +
+  geom_quantile(quantiles = c(0.15, 0.85), size = 2, alpha = 0.5, aes(colour = as.factor(..quantile..))) +
+  labs(x = "Mean Trophic Similitude", y = "Interaction strength (log scale)", color = "Quantiles") +
   theme_bw() +
   theme(panel.grid = element_blank(),
         axis.title = element_text(size = 18, face = "bold"),
         axis.text.x = element_text(size = 15),
         axis.text.y = element_text(size = 15))
+qr_IS_TS
 
 
 #
