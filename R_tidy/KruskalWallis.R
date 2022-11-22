1484ac4938df5ee399072b56b135ca943fb81f95
TomasMarina <tomasimarina@gmail.com>
2022-11-22 17:48:51 -0300

    Added test references

--- R_tidy/KruskalWallis.R
+++ R_tidy/KruskalWallis.R
@@ -11,7 +11,10 @@
 # Load data ----
 
 
+# Kruskal-Wallis test ----
 
-
-
+http://www.sthda.com/english/wiki/kruskal-wallis-test-in-r
+https://stat-methods.com/home/kruskal-wallis-r/
+https://bookdown.org/anshul302/HE802-MGHIHP-Spring2020/Chi2.html#
+https://bookdown.org/danieljcarter/r4steph/non-parametric-tests.html
 
