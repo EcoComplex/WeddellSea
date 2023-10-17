9d8255d6f503ef526deaa46b6dc6298240e5f9c0
TomasMarina <tomasimarina@gmail.com>
2023-10-17 17:14:54 -0300

    Extinction seq

--- R/extinction_sequence_QSS.R
+++ R/extinction_sequence_QSS.R
@@ -8,47 +8,47 @@ library(tictoc)
 # source("R/extinction_fun.R")
 
 ## Load data
-load("Results/net_&_spp_prop.rda")
+load("Results/net_&_spp_prop_sim.rda")
 load("Results/QSS_extinction_dif.rda")
 
 
 ## Extinction sequences ----
 
 # By trophic level
-order_tl <- arrange(spp_attr_all, desc(TLu)) %>% 
-  dplyr::select(TrophicSpecies, TLu)
+order_tl <- arrange(spp_all_prop, desc(TL)) %>% 
+  dplyr::select(TrophicSpecies, TL)
 tl_seq <- order_tl$TrophicSpecies
-tl_seq <- tl_seq[1:400]
+tl_seq <- tl_seq[1:100]
 
 # By degree
-order_deg <- arrange(spp_attr_all, desc(Degree)) %>% 
-  dplyr::select(TrophicSpecies, Degree)
+order_deg <- arrange(spp_all_prop, desc(TotalDegree)) %>% 
+  dplyr::select(TrophicSpecies, TotalDegree)
 deg_seq <- order_deg$TrophicSpecies
-deg_seq <- deg_seq[1:400]
+deg_seq <- deg_seq[1:100]
 
 # By mean interaction strength
-order_is <- arrange(spp_attr_all, desc(AllStrength_mean)) %>% 
-  dplyr::select(TrophicSpecies, AllStrength_mean)
+order_is <- arrange(spp_all_prop, desc(IS_mean)) %>% 
+  dplyr::select(TrophicSpecies, IS_mean)
 is_seq <- order_is$TrophicSpecies
-is_seq <- is_seq[1:400]
+is_seq <- is_seq[1:100]
 
 
 ## Extinction simulations ----
 
 # Extinctions by trophic level
-nsim <- 1000
-tic(paste("Tl Extinctions",nsim))
-QSS_extinction_tl <- extinctions_QSS(g, tl_seq, nsim = nsim, ncores = 48, istrength = TRUE)
+nsim <- 100
+tic(paste("Tl Extinctions", nsim))
+QSS_extinction_tl <- multiweb::calc_QSS_extinctions_seq(g, tl_seq, nsim = nsim, ncores = 48, istrength = TRUE)
 toc()
 QSS_extinction_tl
 
 
 # Extinctions by interaction strength
-QSS_extinction_is <- extinctions_QSS(g, is_seq, nsim = nsim, ncores = 48, istrength = TRUE)
+QSS_extinction_is <- multiweb::calc_QSS_extinctions_seq(g, is_seq, nsim = nsim, ncores = 48, istrength = TRUE)
 QSS_extinction_is
 
 # Extinctions by degree
-QSS_extinction_deg <- extinctions_QSS(g, deg_seq, nsim = nsim, ncores = 48, istrength = TRUE)
+QSS_extinction_deg <- multiweb::calc_QSS_extinctions_seq(g, deg_seq, nsim = nsim, ncores = 48, istrength = TRUE)
 QSS_extinction_deg
 
 # Extinctions topological
@@ -61,6 +61,8 @@ save(QSS_null_comp,QSS_null_comp_raw,QSS_extinction_dif,
      QSS_extinction_is, QSS_extinction_tl, QSS_extinction_deg, 
      file = "Results/QSS_extinction_dif.rda")
 
+save(order_deg, order_tl, order_is, QSS_extinction_deg, QSS_extinction_tl,
+     file = "Results/QSS_extinction_seq_sim.rda")
 
 ## Exploratory plots ----
 
@@ -87,10 +89,10 @@ save(QSS_null_comp,QSS_null_comp_raw,QSS_extinction_dif,
 #
 (plot_del_QSS_tl <- QSS_extinction_tl %>% 
     mutate(Network_prop = Size/490, Ext_prop = (490-Size)/490) %>% 
-    ggplot(aes(x = Ext_prop, y = QSS_median)) +
+    ggplot(aes(x = Connectance, y = QSS_median)) +
     geom_line() +
-    labs(x = "Proportion of deleted spp", y = "QSS median", 
-         title = "Extinction sequence: by decreasing Trophic Level") +
+    #labs(x = "Proportion of deleted spp", y = "QSS median", 
+         #title = "Extinction sequence: by decreasing Trophic Level") +
     theme_classic())
 
 (plot_comp_del_tl <- QSS_extinction_tl %>% 
@@ -105,10 +107,11 @@ save(QSS_null_comp,QSS_null_comp_raw,QSS_extinction_dif,
 #
 (plot_del_QSS_deg <- QSS_extinction_deg %>% 
     mutate(Network_prop = Size/490, Ext_prop = (490-Size)/490) %>% 
-    ggplot(aes(x = Ext_prop, y = QSS_median)) +
+    ggplot(aes(x = Connectance, y = QSS_median)) +
     geom_line() +
-    labs(x = "Proportion of deleted spp", y = "QSS median", 
-         title = "Extinction sequence: decreasing Degree"))
+    #labs(x = "Proportion of deleted spp", y = "QSS median", 
+         #title = "Extinction sequence: decreasing Degree") +
+    theme_classic())
 
 (plot_comp_del_deg <- QSS_extinction_deg %>% 
     mutate(Network_prop = Size/490, Ext_prop = (490-Size)/490) %>% 
