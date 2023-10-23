433f56ef590a21cc1ec77828f5b3313a5ab9d7c2
TomasMarina <tomasimarina@gmail.com>
2023-10-23 15:53:28 -0300

    Tidy script

--- R/extinction_sequence_QSS.R
+++ R/extinction_sequence_QSS.R
@@ -17,7 +17,6 @@ ipak(packages)
 
 
 # Load data ---------------------------------------------------------------
-
 load("Results/net_&_spp_prop_sim.rda")
 load("Results/QSS_extinction_seq_sim.rda")
 #load("Results/QSS_extinction_dif.rda")
@@ -91,85 +90,79 @@ QSS_extinction_is <- multiweb::calc_QSS_extinctions_seq(g, is_seq, nsim = nsim, 
     theme_classic())
 plotly::ggplotly(plot_Conn_QSS)
 
-#
-# Extinction by group porifera and ascidians -----------------------------------------------------
 
+## Functional Group ----
 
 ## Read full taxonomic Classification  
 classall_df <- readRDS("Data/WeddellSea_clasification.rds")
 
-# Extinction by group Porifera and Ascideas -----------------------------------
-
-bygroup <- classall_df %>% filter(phylum == "Porifera" | class =="Ascidiacea") %>% dplyr::select(Taxon) %>% deframe()
-
+### Porifera & Ascidians ----
+por_asc <- classall_df %>% filter(phylum == "Porifera" | class =="Ascidiacea") %>% dplyr::select(Taxon) %>% deframe()
 # Simulation
-
 nsim <- 100
-QSS_extinction_grp <- multiweb::calc_QSS_extinctions_seq(g, bygroup, nsim = nsim, ncores = 8, istrength = TRUE)
-
+QSS_extinction_grp <- multiweb::calc_QSS_extinctions_seq(g, por_asc, nsim=nsim, ncores=8, istrength = TRUE)
+# Plot
 (plot_Conn_QSS <- QSS_extinction_grp %>% 
     mutate(Network_prop = Size/490, Ext_prop = (490-Size)/490) %>% 
     ggplot(aes(x = Ext_prop, y = QSS_median)) +
     geom_line() + geom_smooth(method=lm) +
+    labs(title="Porifera & Ascidians deleted (n=46)", x="Proportion of deleted spp") +
     theme_classic())
 plotly::ggplotly(plot_Conn_QSS)
-
-#
+# Difference with original network
 nsim <- 1000
-grp_dif <- data.frame()
-grp_dif <- bind_rows(grp_dif,
-                     calc_QSS_extinction_dif_grp(g, bygroup,nsim,ncores=8,istrength=TRUE) %>% mutate(deleted_grp="Pori - Asci"))
+por_asc_dif <- data.frame()
+por_asc_dif <- bind_rows(por_asc_dif,
+                     calc_QSS_extinction_dif_grp(g, por_asc, nsim, ncores=8, istrength=TRUE) %>% mutate(deleted_grp="Pori - Asci"))
 
-# Extinction by group Mammals and birds -----------------------------------
-
-
-bygroup <- classall_df %>% filter(class == "Aves" | class =="Mammalia") %>% dplyr::select(Taxon) %>% deframe()
-
+### Mammals & Sea birds ----
+av_mm <- classall_df %>% filter(class == "Aves" | class =="Mammalia") %>% dplyr::select(Taxon) %>% deframe()
 # Simulation
 nsim <- 100
-QSS_extinction_grp_am <- multiweb::calc_QSS_extinctions_seq(g, bygroup, nsim = nsim, ncores = 8, istrength = TRUE)
-
+QSS_extinction_grp_am <- multiweb::calc_QSS_extinctions_seq(g, av_mm, nsim=nsim, ncores=8, istrength = TRUE)
+# Plot
 (plot_Conn_QSS <- QSS_extinction_grp_am %>% 
     mutate(Network_prop = Size/490, Ext_prop = (490-Size)/490) %>% 
     ggplot(aes(x = Ext_prop, y = QSS_median)) +
     geom_line() + geom_smooth(method=lm) +
+    labs(title="Mammals & Sea birds deleted (n=30)", x="Proportion of deleted spp") +
     theme_classic())
 plotly::ggplotly(plot_Conn_QSS)
-
+# Difference with original network
 nsim <- 1000
-grp_dif <- bind_rows(grp_dif,
-                     calc_QSS_extinction_dif_grp(g, bygroup,nsim,ncores=8,istrength=TRUE) %>% mutate(deleted_grp="Mamm - Birds"))
+av_mm_dif <- data.frame()
+av_mm_dif <- bind_rows(av_mm_dif,
+                     calc_QSS_extinction_dif_grp(g, av_mm, nsim, ncores=8, istrength=TRUE) %>% mutate(deleted_grp="Mamm - Birds"))
 
-# Extinction by group Myctopids and Euphasids -----------------------------------
-
-
-bygroup <- classall_df %>% filter(family == "Myctophidae" | family =="Euphausiidae") %>% dplyr::select(Taxon) %>% deframe()
-
+### Myctopids & Euphasids ----
+m_e <- classall_df %>% filter(family == "Myctophidae" | family =="Euphausiidae") %>% dplyr::select(Taxon) %>% deframe()
 # Simulation
 nsim <- 100
-QSS_extinction_grp_me <- multiweb::calc_QSS_extinctions_seq(g, bygroup, nsim = nsim, ncores = 8, istrength = TRUE)
-
+QSS_extinction_grp_me <- multiweb::calc_QSS_extinctions_seq(g, m_e, nsim=nsim, ncores=8, istrength = TRUE)
+# Plot
 (plot_Conn_QSS <- QSS_extinction_grp_me %>% 
     mutate(Network_prop = Size/490, Ext_prop = (490-Size)/490) %>% 
     ggplot(aes(x = Ext_prop, y = QSS_median)) +
     geom_line() + geom_smooth(method=lm) +
+    labs(title="Myctopids & Euphasids deleted (n=10)", x="Proportion of deleted spp") +
     theme_classic())
 plotly::ggplotly(plot_Conn_QSS)
-
-#
-## Extinction total group ----
-#
+# Difference with original network
 nsim <- 1000
-grp_dif <- bind_rows(grp_dif,
-                     calc_QSS_extinction_dif_grp(g, bygroup,nsim,ncores=8,istrength=TRUE) %>% mutate(deleted_grp="Mycto - Eupha"))
-QSS_extinction_grp_dif <- grp_dif
-## Save data ----
+m_e_dif <- data.frame()
+m_e_dif <- bind_rows(m_e_dif,
+                     calc_QSS_extinction_dif_grp(g, m_e, nsim, ncores=8, istrength=TRUE) %>% mutate(deleted_grp="Mycto - Eupha"))
+
+# All data
+dif_all <- bind_rows(por_asc_dif, av_mm_dif, m_e_dif)
+
+# Save results ------------------------------------------------------------
 
 # save(QSS_null_comp,QSS_null_comp_raw,QSS_extinction_dif,
 #      QSS_extinction_is, QSS_extinction_tl, QSS_extinction_deg, 
 #      file = "Results/QSS_extinction_dif.rda")
 
 save(order_deg, order_tl, order_is, QSS_extinction_deg, QSS_extinction_tl, QSS_extinction_is,
-     QSS_extinction_grp,QSS_extinction_grp_am,QSS_extinction_grp_me,QSS_extinction_grp_dif,
-     file = "Results/QSS_extinction_seq_sim.rda")
+     QSS_extinction_grp, QSS_extinction_grp_am, QSS_extinction_grp_me, QSS_extinction_grp_dif,
+     dif_all, file = "Results/QSS_extinction_seq_sim.rda")
 
